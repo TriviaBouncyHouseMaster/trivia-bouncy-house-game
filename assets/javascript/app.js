b6c8b8546048f06a0d81a5c75dcca13e399b0acf
shulme801 <shulme801@gmail.com>
2017-11-16 20:38:50 -0500

    Removed faulty database logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,55 +23,55 @@ $(document).ready(function(){
     /*----------------------------------------------*/
     // Database Code
     /*----------------------------------------------*/
-    var database; 
-    var scores = [];
+    // var database; 
+    // var scores = [];
 
-    // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyDXghKHSKVyem-iUc6YXpbS9pDb2osTCmI",
-        authDomain: "trivia-game-leaderboard.firebaseapp.com",
-        databaseURL: "https://trivia-game-leaderboard.firebaseio.com",
-        projectId: "trivia-game-leaderboard",
-        storageBucket: "",
-        messagingSenderId: "649135365795"
-      };
-      firebase.initializeApp(config);
+    // // Initialize Firebase
+    //   var config = {
+    //     apiKey: "AIzaSyDXghKHSKVyem-iUc6YXpbS9pDb2osTCmI",
+    //     authDomain: "trivia-game-leaderboard.firebaseapp.com",
+    //     databaseURL: "https://trivia-game-leaderboard.firebaseio.com",
+    //     projectId: "trivia-game-leaderboard",
+    //     storageBucket: "",
+    //     messagingSenderId: "649135365795"
+    //   };
+    //   firebase.initializeApp(config);
 
-    database = firebase.database();
+    // database = firebase.database();
 
-    $("#addScore").on("click", function(event) {
-      console.log("HEY, YOU CLICK ON ME")
-      event.preventDefault(); // Don't reset the page!
+    // $("#addScore").on("click", function(event) {
+    //   console.log("HEY, YOU CLICK ON ME")
+    //   event.preventDefault(); // Don't reset the page!
 
-      // Get the initials from form, and number of answers correct from game.
-      var initials = $("#initials").val().trim();
-      var numberCorrect = $("#number-correct").val().trim();
+    //   // Get the initials from form, and number of answers correct from game.
+    //   var initials = $("#initials").val().trim();
+    //   var numberCorrect = $("#number-correct").val().trim();
       
-      // Hardcode test database
-      // var initials = "JMC";
-      // var numberCorrect = 20000;
+    //   // Hardcode test database
+    //   // var initials = "JMC";
+    //   // var numberCorrect = 20000;
 
-      database.ref().push({
-        initials: initials,
-        numberCorrect: numberCorrect,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      })
-    });
+    //   database.ref().push({
+    //     initials: initials,
+    //     numberCorrect: numberCorrect,
+    //     dateAdded: firebase.database.ServerValue.TIMESTAMP
+    //   })
+    // });
 
-    database.ref().on('value', function(snapshot) {
-        console.log("I got here");
-        scores = [];
-        $("#trains").empty();
-        snapshot.forEach(function(childSnapshot) {
-          var childData = childSnapshot.val();
-          scores.push({
-            initials: initials,
-            numberCorrect: numberCorrect,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-          });
-        });
-        scores.forEach(outputOneRow);
-    });   
+    // database.ref().on('value', function(snapshot) {
+    //     console.log("I got here");
+    //     scores = [];
+    //     $("#trains").empty();
+    //     snapshot.forEach(function(childSnapshot) {
+    //       var childData = childSnapshot.val();
+    //       scores.push({
+    //         initials: initials,
+    //         numberCorrect: numberCorrect,
+    //         dateAdded: firebase.database.ServerValue.TIMESTAMP
+    //       });
+    //     });
+    //     scores.forEach(outputOneRow);
+    // });   
     /*----------------------------------------------*/
 
     function startTimer() {
@@ -339,7 +339,15 @@ $(document).ready(function(){
                             getQandA();
                             
                             break;
-                        
+
+                        case "resetGame":
+                            console.log("Found resetGame");
+                            break;
+
+                        case "gameScore":
+                            console.log("Found gameScore");
+                            break;
+
                         default:
                             
                             alert("Unknown button!!");
